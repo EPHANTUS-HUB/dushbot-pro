72f347190b1dbbfaac6dbda0f14d63d27b760a5a
EPHANTUS-HUB <ephantuslangat5@gmail.com>
2025-04-29 20:04:07 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const moment = require('moment');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        headless: true
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-accelerated-2d-canvas",
+            "--no-first-run",
+            "--no-zygote",
+            "--single-process",
+            "--disable-gpu"
+        ]
     }
 });
 
@@ -29,8 +38,8 @@ client.on('ready', () => {
 
 client.on('message', async msg => {
     const text = msg.body.toLowerCase();
-
     const greetings = ['hi', 'hello', 'hii', 'hey', 'yo', 'yoo', 'hiii'];
+
     if (greetings.includes(text)) {
         return msg.reply('Hey there! I’m DushBot — how can I help you? Type *menu* to see commands.');
     }
